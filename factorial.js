fbcb274d77ab41b109f8101505e19c325f4da0dc
Shakti Singh chauhan <57222868+Shakti993@users.noreply.github.com>
2020-01-12 21:15:13 +0530

    Add files via upload

---
+++ factorial.js
@@ -0,0 +1,18 @@
+console.log("Shakti");
+
+const myname = name => {
+    return 'Hi my name is  ' + name;
+}
+
+console.log(myname('shakti'));
+
+const mynewname = name => `Hey ${name}`;
+
+console.log(mynewname('shakti'));
+
+console.log("%s",'HURRAY SHAKTI');
+process.stdout.write("Wow Shakti ");
+
+ 
+
+  
